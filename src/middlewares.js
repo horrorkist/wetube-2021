3ee02879fca2cc4a9bad5f99271fd688c9ef5a85
horrorkist <horrorkist@gmail.com>
2021-08-25 20:53:24 +0900

    add AWS s3 bucket

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,18 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: preocess.env.AWS_SECRET,
+  },
+});
+
+const multerUploader = multerS3({
+  s3: s3,
+  bucket: "wetube-horrorkist",
+});
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -25,8 +39,10 @@ export const publicOnlyMiddleware = (req, res, next) => {
 export const avatarUpload = multer({
   dest: "uploads/avatars/",
   limit: 1024 * 1024 * 3,
+  storage: multerUploader,
 });
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limit: 1024 * 1024 * 10,
+  storage: multerUploader,
 });
